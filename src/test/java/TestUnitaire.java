e48d92be291f9a52eff273343efb50fda4facccf
ZakapiaFR <erwanpauloin@gmail.com>
2025-09-12 14:31:05 +0200

    Ajout du code de remove.java et modification du test unitaire.

--- src/test/java/TestUnitaire.java
+++ src/test/java/TestUnitaire.java
@@ -1,11 +1,16 @@
-package src.test.java;
 
-import src.main.java.Add;
 
 public class TestUnitaire {
     public static void main(String[] args) {
-        int resultat = Add.ajout(12, 13);
-        if (resultat == 25) {
+        int resultat1 = Add.ajout(12, 13);
+        if (resultat1 == 25) {
+            System.out.println("Test réussi !");
+        } else {
+            System.out.println("Test échoué !");
+        }
+
+        int resultat2 = Remove.retrait(14, 11);
+        if (resultat2 == 3) {
             System.out.println("Test réussi !");
         } else {
             System.out.println("Test échoué !");
